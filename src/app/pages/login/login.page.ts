35d23618a64dbd5e0b00ad4cdf94c7920ba68f3b
Brayan Vargas <vargasalarcon2001@gmail.com>
2025-12-02 02:10:14 -0500

    Formulario de registro y foro mejorados, navegación optimizada en el dashboard, y actualización de iconos para una mejor experiencia de usuario. 	modified:   src/app/pages/dashboard/dashboard.page.html 	modified:   src/app/pages/dashboard/dashboard.page.scss 	modified:   src/app/pages/dashboard/dashboard.page.ts 	modified:   src/app/pages/formulario-emociones/formulario-emociones.page.html 	modified:   src/app/pages/formulario-emociones/formulario-emociones.page.scss 	modified:   src/app/pages/foro/foro.page.html 	modified:   src/app/pages/foro/foro.page.scss 	modified:   src/app/pages/login/login.page.html 	modified:   src/app/pages/login/login.page.scss 	modified:   src/app/pages/login/login.page.ts 	modified:   src/app/pages/notificaciones/notificaciones.page.html 	modified:   src/app/pages/register/register.page.html 	modified:   src/app/pages/register/register.page.scss 	modified:   src/app/pages/register/register.page.ts 	modified:   src/app/tabs/tabs.page.html 	modified:   src/app/tabs/tabs.page.scss 	new file:   src/assets/icon/Ayuda.png 	new file:   src/assets/icon/Home.png 	new file:   src/assets/icon/Meditacion.png 	new file:   src/assets/icon/Notificacion.png 	new file:   src/assets/icon/Perfil-usuario.png 	deleted:    src/assets/icon/ayuda-psicologica.png 	deleted:    src/assets/icon/casa.png 	new file:   src/assets/icon/comentario-alt.png 	deleted:    src/assets/icon/favicon.png 	deleted:    src/assets/icon/foro-2.png 	deleted:    src/assets/icon/foro.png 	deleted:    src/assets/icon/llamada.png 	deleted:    src/assets/icon/meditacion.1.png 	deleted:    src/assets/icon/meditacioncorazon.png 	deleted:    src/assets/icon/notificaciones.png 	deleted:    src/assets/icon/perfil.png 	deleted:    src/assets/shapes.svg

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
+import { IonNav } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule],
+  imports: [IonicModule, CommonModule, FormsModule,IonNav],
 })
 export class LoginPage {
 
